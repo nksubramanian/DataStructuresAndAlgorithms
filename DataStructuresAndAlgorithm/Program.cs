3aae1f4d1335811c140c49c420af9f760a2275c5
subramanian <subramanian.enkay@gmail.com>
2025-10-20 13:09:30 +0530

    added class

--- DataStructuresAndAlgorithm/Program.cs
+++ DataStructuresAndAlgorithm/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+void printMyQueue(Queue<int> queue)
+{
+    var count = queue.Count;
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(queue.Dequeue());
+    }
+
+}
+
+
+void reverseMyQueue(Queue<int> queue)
+{
+    if (queue.Count <= 1)
+    {
+        return;
+    }
+    else
+    {
+        var element = queue.Dequeue();
+        reverseMyQueue(queue);
+        queue.Enqueue(element);
+    }
+}
+
+
+Queue<int> myQueue = new Queue<int>();
+myQueue.Enqueue(1);
+myQueue.Enqueue(2);
+myQueue.Enqueue(3);
+reverseMyQueue(myQueue);
+printMyQueue(myQueue);
+
