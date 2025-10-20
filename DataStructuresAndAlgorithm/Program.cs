00dc1464f48e5ca51bb873a0e07550462711f1a0
subramanian <subramanian.enkay@gmail.com>
2025-10-20 13:12:34 +0530

    queue reversed

--- DataStructuresAndAlgorithm/Program.cs
+++ DataStructuresAndAlgorithm/Program.cs
@@ -1,36 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DataStructuresAndAlgorithm;
+
 Console.WriteLine("Hello, World!");
 
-void printMyQueue(Queue<int> queue)
-{
-    var count = queue.Count;
-    for (int i = 0; i < count; i++)
-    {
-        Console.WriteLine(queue.Dequeue());
-    }
-
-}
+QueueReversal queueReversal = new QueueReversal();
+queueReversal.main();
 
 
-void reverseMyQueue(Queue<int> queue)
-{
-    if (queue.Count <= 1)
-    {
-        return;
-    }
-    else
-    {
-        var element = queue.Dequeue();
-        reverseMyQueue(queue);
-        queue.Enqueue(element);
-    }
-}
-
-
-Queue<int> myQueue = new Queue<int>();
-myQueue.Enqueue(1);
-myQueue.Enqueue(2);
-myQueue.Enqueue(3);
-reverseMyQueue(myQueue);
-printMyQueue(myQueue);
 
---
+++ DataStructuresAndAlgorithm/QueueReversal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithm
+{
+    internal class QueueReversal
+    {
+        void printMyQueue(Queue<int> queue)
+        {
+            var count = queue.Count;
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+
+        void reverseMyQueue(Queue<int> queue)
+        {
+            if (queue.Count <= 1)
+            {
+                return;
+            }
+            else
+            {
+                var element = queue.Dequeue();
+                reverseMyQueue(queue);
+                queue.Enqueue(element);
+            }
+        }
+
+        public void main()
+        {
+            Queue<int> myQueue = new Queue<int>();
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);
+            myQueue.Enqueue(3);
+            reverseMyQueue(myQueue);
+            printMyQueue(myQueue);
+        }
+
+    }
+}
